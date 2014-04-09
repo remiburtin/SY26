087f39f39ae5cefdadcd4aba266697eb39371e85
Rémi Burtin <remi.burtin@etu.utc.fr>
2014-04-09 22:41:37 +0200

    Ajout log script codage arithmétique

--- TP2/arith.m
+++ TP2/arith.m
@@ -1,6 +1,7 @@
-function code = arith(path)
+function compression = arith(path)
 
 % matrice de l'image
+disp(['Lecture de l''image ' path]);
 img = imread(path);
 
 % initialisation vecteurs
@@ -10,6 +11,7 @@ histogramme = zeros(1,256); % occurences par niveaux de gris
 composantes = size(img, 3); % Taille de la 3eme dimension 
 
 if (composantes == 3) % conversion en gris si image couleur
+    disp('Image couleur : conversion en gris');
     img = rgb2gray(img);
 end
 
@@ -20,8 +22,6 @@ list=reshape(double(img), 1, dim); % images en ligne (vecteur)
 for i=1:dim,
     histogramme(round(list(i))) = histogramme(round(list(i))) + 1;
 end
-
-histogramme
 
 [val, index] = find (histogramme ~= 0) ;
 histo_0 = histogramme(index);
@@ -38,9 +38,14 @@ for i=1:dim,
     list_0(i) = corres(list(i));
 end
 
+disp('Encodage de l''image');
+tic % calcul du temps d'encodage/decodage
 code = arithenco(list_0, histo_0);
 
+disp('Decodage de l''image');
 deco = arithdeco(code, histo_0, dim);
+time = toc;
+disp(['Duree encodage/decodage : ' num2str(time) 's.']);
 
 deco_2 = zeros(1,dim);
 
@@ -51,6 +56,10 @@ end
 imagedeco = reshape(deco_2, size(img,1), size(img,2));
 
 imshow(imagedeco/256);
+
+compression = 1-length(code)/(dim*8);
+disp(['Taux de compression : 1 - taille finale / taille initiale = ' num2str(compression)]);
+
 
 return;
 
