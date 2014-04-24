6cbcf3e5c479a1a5dff4f1f5bfc7cca8046487e3
fouge <cyril.fougeray@gmail.com>
2014-04-24 09:47:26 +0200

    Partie 2_1 2_2

--- TP3/lloyd_max_2_1.m
+++ TP3/lloyd_max_2_1.m
@@ -11,37 +11,34 @@ function [image_quant disto] = quant_lloyd(image, debit)
     
     % Construction des partitions
     partition = linspace(0,255, 2^debit+1);
-
     % Creation des valeurs de reconstruction
     for i=1:(2^debit),
         codebook(i) = (partition(i) + partition(i+1)) / 2 ;
     end
     
     % Initialisation condition d'arret
-    oldDisto = 9999;
-    diffDisto = 9999;
+    oldDisto = 9999; % doit etre >> à la première disto calculee
+    diffDisto = 9999; % Doit etre > à 0.1 pour entrer dans la boucle
     
     % Condition de convergence
     while diffDisto > 0,1;
-        %calcul des ri
-        for j=1:(2^debit)
+        
+        for j=1:(2^debit) %calcul des ri
+            % v = index des éléments appartenant à l'intervalle j dans list
             v = find(list >= partition(j) & list <= partition(j+1));
-            if (isempty(v))
+            if (isempty(v)) % intervalle vide
                 codebook(j) = (partition(j) + partition(j + 1)) / 2;
             else
                 codebook(j) = mean(list(v));
             end
         end
         
-        %calcul des di
-        for j=2:(2^debit)
+        for j=2:(2^debit) %calcul des di
             partition(j) = (codebook(j) + codebook(j - 1))/2;
         end
         
         % Quantification
         [~, image_quant] = quantiz(list, partition(2:2^debit), codebook);
-    
-    
         % Calcul de la distorsion
         disto = mean((list-image_quant).^2);
         
