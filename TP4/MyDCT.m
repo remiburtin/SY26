a85c7bcc5139d3a91c533013fe097e59df6b4af9
fouge <cyril.fougeray@gmail.com>
2014-04-26 18:22:43 +0200

    TP04

---
+++ TP4/MyDCT.m
@@ -0,0 +1,16 @@
+function D = MyDCT(B)
+
+X = zeros(8,8);
+Y = zeros(8,8);
+C = [1/sqrt(2) 1 1 1 1 1 1 1];
+
+for i=0:7,
+    for j=0:7,
+        X(i+1,j+1) = C(i+1)/2*cos(((2*(j)+1)*pi*(i))/16);
+    end;
+end;
+
+Y = X;
+D = X * B * Y';
+
+end
