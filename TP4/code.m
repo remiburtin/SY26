df8e5a8cd0900c194bb542053ac01f08b579d6fa
Rémi Burtin <remi.burtin@etu.utc.fr>
2014-05-29 14:34:02 +0200

    Fix code.m lorsque la matrice quantifiée ne contient que des zéros

--- TP4/code.m
+++ TP4/code.m
@@ -1,10 +1,6 @@
 function A= code(Vzig,diff,nb)
-
 % Indexes des coefficients non nuls.
 v_non_nul = find(Vzig ~= 0);
-
-% Initialisation du vecteur contenant le bloc encode.
-A = zeros(1,length(v_non_nul)*3-1+2);
 
 % Initialisation des deux premieres valeurs deu bloc encode.
 A(1) = nb;
@@ -16,5 +12,8 @@ for i=2:length(v_non_nul),
     A(3*(i-1)+1) = nb;
     A(3*(i-1)+2) = Vzig(v_non_nul(i));
 end;
+
+% Fin de bloc
+A = [A 0 0];
 
 end
