fad444781c4016bb72d2901afa76e704e473cea0
Rémi Burtin <remi.burtin@etu.utc.fr>
2014-06-06 20:56:02 +0200

    Changement calcul msd pour que ca marche sur des matrices non carrées

--- TP5/compute_msd.m
+++ TP5/compute_msd.m
@@ -1,10 +1,11 @@
 function msd = compute_msd( current_block, block )
     M = size(block,1);
-    msd = 1/M^2;
+    N = size(block,2);
+    msd = 1/(M*N);
     somme = 0;
     
     for i=1:M,
-        for j=1:M,
+        for j=1:N,
             somme = somme + (current_block(i,j)-block(i,j))^2;
         end;
     end;
