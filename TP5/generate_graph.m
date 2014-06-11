9cca6f6ec2e5c84f25abf17d7a83f344ee908492
Rémi Burtin <remi.burtin@etu.utc.fr>
2014-06-11 07:39:51 +0200

    Ajout courbe temps de calcul

--- TP5/generate_graph.m
+++ TP5/generate_graph.m
@@ -4,17 +4,25 @@ Ns = [1,2,3];
 i1 = imread('car1.png');
 i2 = imread('car2.png');
 
-res = zeros(length(Ns),length(Ws));
-
+msd = zeros(length(Ns),length(Ws));
+time = zeros(length(Ns),length(Ws));
 for i=1:length(Ns),
     for j=1:length(Ws),
-        [time, output_image, error, res(i,j)] = block_matching_encode(i1,i2,Ns(i),Ws(j));
+        [time(i,j), output_image, error, msd(i,j)] = block_matching_encode(i1,i2,Ns(i),Ws(j));
     end;
 end;
 
-plot(2*Ns+1,res)
+subplot(1,2,1)
+plot(2*Ns+1,msd)
 set(gca,'XTick',2*Ns+1)
 xlabel('M')
 ylabel('MSD')
 title('Evolution du MSD en fonction de la taille des blocs et de la fenetre de recherche')
+legend('w = 5','w = 10', 'w = 15')
+subplot(1,2,2)
+plot(2*Ns+1,time)
+set(gca,'XTick',2*Ns+1)
+xlabel('M')
+ylabel('Temps')
+title('Evolution du temps de traitement en fonction de la taille des blocs et de la fenetre de recherche')
 legend('w = 5','w = 10', 'w = 15')
