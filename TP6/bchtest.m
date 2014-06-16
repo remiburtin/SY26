bedd62f7fe09111d9eadeb0396aee2859e360d86
Rémi Burtin <remi.burtin@etu.utc.fr>
2014-06-16 21:11:26 +0200

    finition code de test bch

--- TP6/bchtest.m
+++ TP6/bchtest.m
@@ -1,5 +1,22 @@
-[gen, t] = bchgenpoly(31,16);
+m = 5;
 
-rd = round(rand(1,16));
+%Longueur du mot code
+n = 2^m-1;
 
-H = cyclgen(31, gen)
+%Longueur du message
+k = 16;
+
+%Message aleatoire
+msg = gf(randi([0 1],1,k));
+
+%Calcul polynome generateur
+[gen, t] = bchgenpoly(n,k);
+
+%Codage du message
+code = bchenc(msg,n,k);
+
+%Ajout de t erreurs aleatoires
+code_bruite = code + randerr(1,n,t);
+
+%Decodage
+[msg_decode,nb_err] = bchdec(code_bruite,n,k);
