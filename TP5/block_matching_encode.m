81d2f8761ed21d19e164cdcbbada0e785f396c79
fouge <cyril.fougeray@gmail.com>
2014-06-18 16:31:34 +0200

    [TP5] Calcul de la fenetre de recherche

--- TP5/block_matching_encode.m
+++ TP5/block_matching_encode.m
@@ -7,12 +7,12 @@ function [ time, output_image, error, msd ] = block_matching_encode( img_ref, im
     
     %padding de l'image (on ajoute des 0 pour que la largeur/hauteur soit
     %multiple de M
-    pad_y = mod(size(img,1),M);
+    pad_y = mod(mod(size(img,1),M), M);
     if pad_y > 0
         pad_y = M - pad_y;
     end;
     
-    pad_x = mod(size(img,2),M);
+    pad_x = mod(mod(size(img,2),M), M);
     if pad_x > 0
         pad_x = M - pad_x;
     end;
@@ -20,7 +20,7 @@ function [ time, output_image, error, msd ] = block_matching_encode( img_ref, im
     img = double(padarray(img,[pad_y pad_x],0,'post'));
     img_ref = double(padarray(img_ref,[pad_y pad_x],0,'post'));
     
-    %affichage de la difference en tre l'image courante et l'image de
+    %affichage de la difference entre l'image courante et l'image de
     %reference
     figure('name','Diff current vs ref');
     imshow(((img-img_ref)+128)/255);
@@ -34,11 +34,12 @@ function [ time, output_image, error, msd ] = block_matching_encode( img_ref, im
     tic
     
     k=1;
-    for i=1:M:size(img,1),
+ 
+    for i=1:M:size(img,1), % for i=1 to i=height, with a step size of M.
         l=1;
         for j=1:M:size(img,2),
             %extraction bloc courant
-            block = img(i:(i+M)-1,j:(j+M)-1);
+            block = img(i:i+M-1,j:j+M-1);
             
             %calcul de la fenetre de recherche
             [window, orig_x, orig_y] = search_window(img_ref,W,M,i,j);
