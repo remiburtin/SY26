babe35a5ac3321f1e58f086964d93f2297aad1e6
fouge <cyril.fougeray@gmail.com>
2014-06-19 11:05:39 +0200

    [TP6] Intro (copié collé) + decodage Hamming

--- TP6/hamdecode.m
+++ TP6/hamdecode.m
@@ -1,17 +1,21 @@
 function decode = hamdecode(r,H)
-% Calcul du syndrome
 
+% Calcul du syndrome
 s = mod(r*H', 2);
+% Conversion de s (syndrome),
 i = bi2de(s,2,'left-msb');
 
-% Calcul du vecteur d'erreur
-i = i+1;
-table = syndtable(H);
+if i~= 0
+    % Calcul du vecteur d'erreur
+    i = i+1;
+    table = syndtable(H)
 
-e = table(i,:);
+    e = table(i,:);
 
-% Mot decode suppose emis
-
-decode = xor(r,e);
+    % Mot decode suppose emis
+    decode = xor(r,e);
+else
+    decode = r;
+end;
 
 end
